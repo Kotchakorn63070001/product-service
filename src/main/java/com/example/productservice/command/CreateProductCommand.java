953f518eb31f87ece5e2e0d644519f0f751b9984
Kotchakorn63070001 <63070001@kmitl.ac.th>
2022-10-18 11:24:57 +0700

    update lab10

--- src/main/java/com/example/productservice/command/CreateProductCommand.java
+++ src/main/java/com/example/productservice/command/CreateProductCommand.java
@@ -8,7 +8,8 @@ import java.math.BigDecimal;
 
 @Builder
 @Data
-public class CreateProductCommand {
+public class
+CreateProductCommand {
 
     @TargetAggregateIdentifier
     private final String productId;
